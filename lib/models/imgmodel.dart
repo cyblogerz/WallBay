cabf94ed9cb9fd13b45cd6b58e400f6b4cb9fd6a
Pranav Ajay <pranavajay2002@gmail.com>
2022-06-28 14:45:09 +0530

    Added skeleton files

---
+++ lib/models/imgmodel.dart
@@ -0,0 +1,13 @@
+class Bg {
+  final String url;
+  final String category;
+  final int id;
+  final String name;
+
+  Bg({
+    required this.category,
+    required this.id,
+    required this.name,
+    required this.url,
+  });
+}
