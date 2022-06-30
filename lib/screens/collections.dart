aea544daf515f7df2e99546d74d1cbc592364719
Pranav Ajay <pranavajay2002@gmail.com>
2022-06-30 15:40:44 +0530

    Update collections.dart

--- lib/screens/collections.dart
+++ lib/screens/collections.dart
@@ -11,6 +11,23 @@ class CollectionsPage extends StatelessWidget {
         itemBuilder: ((context, index) => Padding(
             padding: EdgeInsets.all(8),
             child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    "FLOWERS",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10.0),
+                    child: Text(
+                      "24",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+                    ),
+                  )
+                ],
+              ),
               decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(32), color: Colors.red),
               width: double.infinity,
